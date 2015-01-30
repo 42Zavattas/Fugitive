061d99b259a5fa15a871d49c4a8194df373c97aa
Aperçu <bgronon@gmail.com>
2015-01-30 17:56:18 +0100

    fix(link): expire date stamped

--- server/api/link/link.controller.js
+++ server/api/link/link.controller.js
@@ -22,7 +22,7 @@ exports.reroute = function (req, res) {
 
       var dest = link.dst;
 
-      if (link.num === 0 || (link.exp && new Date().getTime() > link.exp)) {
+      if (link.num === 0 || (link.exp && new Date().getTime() > link.exp.getTime())) {
         // The link is expired, delete or redirect if set
         if (link.rpl) {
           dest = link.rpl;
