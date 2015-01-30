da839cabdb5f4174b11187089ef4f47b442426de
Aperçu <bgronon@gmail.com>
2015-01-30 03:57:21 +0100

    style(auth): just because the auth needs some fancy spinner

--- client/views/auth/auth.controller.js
+++ client/views/auth/auth.controller.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('fugitive')
-  .controller('AuthCtrl', function ($http, $location, $cookieStore) {
+  .controller('AuthCtrl', function ($http, $location, $timeout, $cookieStore) {
 
     $http.post('/auth', { uuid: $location.$$url.substr(6) })
       .then(function (res) {
         $cookieStore.put('token', res.data.token);
-        $location.path('/');
+        $timeout(function () {$location.path('/');}, 500);
       });
 
   });
