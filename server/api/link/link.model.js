82c716986440d31b73389197c22fbcc76e005c46
Aperçu <bgronon@gmail.com>
2015-01-30 18:05:09 +0100

    style(link): minor

--- server/api/link/link.model.js
+++ server/api/link/link.model.js
@@ -7,7 +7,7 @@ var LinkSchema = new Schema({
   src : String,
   dst : String,
   user: { type: Schema.ObjectId, ref: 'User', default: null },
-  exp : { type: Date, default: null },
+  exp : { type: String, default: null },
   num : { type: Number, default: -1 },
   geo : [
     { country: String, rpl: String }
